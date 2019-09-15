5390321b7a6729b35660a9596e05e466258079f1
Ronald Bosma <ronaldbosma@gmail.com>
2019-09-15 17:39:26 +0200

    Several improvements.
    Log error and exit script with code 1 if release or installer not found.
    Upload installer log files.
    Log error and exit script with code 1 if installer failed.

--- InstallNetCoreRuntimeAndHostingTask/task.ps1
+++ InstallNetCoreRuntimeAndHostingTask/task.ps1
@@ -10,7 +10,7 @@ try
 
     $dotNetVersion = Get-VstsInput -Name version -Require
     $norestart = Get-VstsInput -Name norestart -Require
-
+    
     $fileName = "dotnet-hosting-win.exe"
     $releasesJSONURL = "https://dotnetcli.blob.core.windows.net/dotnet/release-metadata/" + $dotNetVersion + "/releases.json"
     $webClient = new-Object System.Net.WebClient
@@ -18,7 +18,7 @@ try
     # Load releases.json
     Write-Host Load release data from: $releasesJSONURL
     $releases = $webClient.DownloadString($releasesJSONURL) | ConvertFrom-Json
-    
+
     Write-Host Latest Release Version: $releases.'latest-release'
     Write-Host Latest Release Date: $releases.'latest-release-date'
 
@@ -28,7 +28,8 @@ try
         
     if ($latestRelease -eq $null)
     {
-        throw "No latest release found"
+        Write-Host "##vso[task.logissue type=error;]No latest release found"
+        [Environment]::Exit(1)
     }
 
 
@@ -37,7 +38,8 @@ try
         
     if ($file -eq $null)
     {
-        throw "File $fileName not found in latest release"
+        Write-Host "##vso[task.logissue type=error;]File $fileName not found in latest release"
+        [Environment]::Exit(1)
     }
 
     $installerFolder = Join-Path "$(System.DefaultWorkingDirectory)" $releases.'latest-release'
@@ -62,8 +64,25 @@ try
     }
     Write-Host Execute $installerFilePath with the following arguments: $installationArguments
     Write-Host Executing...
-    Start-Process -FilePath $installerFilePath -ArgumentList $installationArguments -Wait
-    Write-Host Installation completed
+    $process = Start-Process -FilePath $installerFilePath -ArgumentList $installationArguments -Wait -PassThru
+    Write-Host Installer completed with exitcode: $process.ExitCode
+
+
+    ## Upload installation logs
+    $logFiles = Get-ChildItem $logFolder -Filter *.log
+    foreach ($logFile in $logFiles) {
+        $logFilePath = $logFile.FullName
+        Write-Host Upload installation log: $logFilePath
+        Write-Host "##vso[task.uploadfile]$logFilePath"
+    }
+
+
+    # Exit with error if installation failed
+    if ($process.ExitCode -ne 0) {
+        $exitCode = $process.ExitCode
+        Write-Host "##vso[task.logissue type=error;]Installation failed with code: $exitCode. See attached logs for more details."
+        [Environment]::Exit(1)
+    }
 }
 finally
 {
