6d435b5b171331798fcbb725221e267445beef7b
Ronald Bosma <ronaldbosma@gmail.com>
2019-10-19 16:24:21 +0200

    Moved install of .net core to seperate function

--- InstallNetCoreRuntimeAndHostingTask/functions.ps1
+++ InstallNetCoreRuntimeAndHostingTask/functions.ps1
@@ -59,3 +59,43 @@ function Download-DotNetCoreInstaller([string]$dotNetVersion, [bool]$useProxy, [
 
     return $outputFilePath
 }
+
+function Install-DotNetCore([string]$installerFilePath, [bool]$norestart)
+{
+    $installerFolder = Split-Path $installerFilePath -Parent
+    $fileName = Split-Path $installerFilePath -Leaf
+
+    # Create log folder
+    $logFolder = Join-Path $installerFolder "logs"
+    $logFilePath = Join-Path $logFolder "$fileName.log"
+    New-Item -Path $logFolder -ItemType Directory | Out-Null
+
+
+    # Execute installer
+    $installationArguments = "/passive /log $logFilePath"
+    if ($norestart)
+    {
+        $installationArguments += " /norestart"
+    }
+    Write-Host Execute $installerFilePath with the following arguments: $installationArguments
+    Write-Host Executing installer. This could take a few minutes...
+    $process = Start-Process -FilePath $installerFilePath -ArgumentList $installationArguments -Wait -PassThru
+    Write-Host Installer completed with exitcode: $process.ExitCode
+
+
+    ## Upload installation logs
+    $logFiles = Get-ChildItem $logFolder -Filter *.log
+    foreach ($logFile in $logFiles) {
+        $logFilePath = $logFile.FullName
+        Write-Host Upload installation log: $logFilePath
+        Write-Host "##vso[task.uploadfile]$logFilePath"
+    }
+
+
+    # Exit with error if installation failed
+    if ($process.ExitCode -ne 0) {
+        $exitCode = $process.ExitCode
+        Write-Host "##vso[task.logissue type=error;]Installation failed with code: $exitCode. See attached logs for more details."
+        [Environment]::Exit(1)
+    }
+}
--- InstallNetCoreRuntimeAndHostingTask/task.ps1
+++ InstallNetCoreRuntimeAndHostingTask/task.ps1
@@ -10,6 +10,7 @@ try
     . "$PSScriptRoot\functions.ps1"
 
     $dotNetVersion = Get-VstsInput -Name version -Require
+    $norestart = Get-VstsInput -Name norestart -Require
     $useProxy = Get-VstsInput -Name useProxy -Require
     $proxyServerAddress = ""
     if ($useProxy)
@@ -22,46 +23,7 @@ try
     $outputFilePath = Join-Path $workingDirectory "dotnet-hosting-win.exe"
 
     $installerFilePath = Download-DotNetCoreInstaller -dotNetVersion $dotNetVersion -useProxy $useProxy -proxyServerAddress $proxyServerAddress -outputFilePath $outputFilePath
-
-    
-    $norestart = Get-VstsInput -Name norestart -Require
-
-    $installerFolder = Split-Path $installerFilePath -Parent
-    $fileName = Split-Path $installerFilePath -Leaf
-
-    # Create log folder
-    $logFolder = Join-Path $installerFolder "logs"
-    $logFilePath = Join-Path $logFolder "$fileName.log"
-    New-Item -Path $logFolder -ItemType Directory | Out-Null
-
-
-    # Execute installer
-    $installationArguments = "/passive /log $logFilePath"
-    if ($norestart)
-    {
-        $installationArguments += " /norestart"
-    }
-    Write-Host Execute $installerFilePath with the following arguments: $installationArguments
-    Write-Host Executing installer. This could take a few minutes...
-    $process = Start-Process -FilePath $installerFilePath -ArgumentList $installationArguments -Wait -PassThru
-    Write-Host Installer completed with exitcode: $process.ExitCode
-
-
-    ## Upload installation logs
-    $logFiles = Get-ChildItem $logFolder -Filter *.log
-    foreach ($logFile in $logFiles) {
-        $logFilePath = $logFile.FullName
-        Write-Host Upload installation log: $logFilePath
-        Write-Host "##vso[task.uploadfile]$logFilePath"
-    }
-
-
-    # Exit with error if installation failed
-    if ($process.ExitCode -ne 0) {
-        $exitCode = $process.ExitCode
-        Write-Host "##vso[task.logissue type=error;]Installation failed with code: $exitCode. See attached logs for more details."
-        [Environment]::Exit(1)
-    }
+    Install-DotNetCore -installerFilePath $installerFilePath -norestart $norestart
 }
 finally
 {
