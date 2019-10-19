ee8508f7710c4254c02a9e341c8e9a6505771f9d
Ronald Bosma <ronaldbosma@gmail.com>
2019-10-19 17:22:28 +0200

    test with boolean params

--- InstallNetCoreRuntimeAndHostingTask/task.ps1
+++ InstallNetCoreRuntimeAndHostingTask/task.ps1
@@ -11,18 +11,28 @@ try
 
     $dotNetVersion = Get-VstsInput -Name version -Require
     $norestart = Get-VstsInput -Name norestart -AsBool -Require
-    $useProxy = Get-VstsInput -Name useProxy -AsBool -Require
-    $proxyServerAddress = ""
-    if ($useProxy -eq $true) {
-        $proxyServerAddress = Get-VstsInput -Name proxyServerAddress -Require
+    $useProxy = Get-VstsInput -Name useProxy -Require
+
+
+    if ($norestart -eq $true) {
+        write-host "norestart true"
+    } else {
+        write-host "norestart false"
     }
-
-    $workingDirectory = Get-VstsTaskVariable -Name "System.DefaultWorkingDirectory"
-    $workingDirectory = Join-Path $workingDirectory $dotNetVersion
-    $outputFilePath = Join-Path $workingDirectory "dotnet-hosting-win.exe"
-
-    $installerFilePath = Get-DotNetCoreInstaller -dotNetVersion $dotNetVersion -useProxy $useProxy -proxyServerAddress $proxyServerAddress -outputFilePath $outputFilePath
-    Install-DotNetCore -installerFilePath $installerFilePath -norestart $norestart
+    
+    if ($useProxy -eq $true) {
+        write-host "useProxy true"
+    } else {
+        write-host "useProxy false"
+    }
+    
+    if ($useProxy -eq 'true') {
+        write-host "useProxy string true"
+    } elseif ($useProxy -eq 'false') {
+        write-host "useProxy string false"
+    } else {
+        write-host "useProxy string else"
+    }
 }
 finally
 {
