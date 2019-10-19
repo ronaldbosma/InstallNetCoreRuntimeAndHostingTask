7257710ac72a0a61535ded4b9ce800636bcfdde9
Ronald Bosma <ronaldbosma@gmail.com>
2019-10-19 17:27:36 +0200

    removed useproxy

--- InstallNetCoreRuntimeAndHostingTask/task.ps1
+++ InstallNetCoreRuntimeAndHostingTask/task.ps1
@@ -11,27 +11,12 @@ try
 
     $dotNetVersion = Get-VstsInput -Name version -Require
     $norestart = Get-VstsInput -Name norestart -AsBool -Require
-    $useProxy = Get-VstsInput -Name useProxy -Require
 
 
     if ($norestart -eq $true) {
         write-host "norestart true"
     } else {
         write-host "norestart false"
-    }
-    
-    if ($useProxy -eq $true) {
-        write-host "useProxy true"
-    } else {
-        write-host "useProxy false"
-    }
-    
-    if ($useProxy -eq 'true') {
-        write-host "useProxy string true"
-    } elseif ($useProxy -eq 'false') {
-        write-host "useProxy string false"
-    } else {
-        write-host "useProxy string else"
     }
 }
 finally
