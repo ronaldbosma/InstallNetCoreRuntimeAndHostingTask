53596ccc968535de4c0a3491e4bfdd8c9cea0a04
Ronald Bosma <ronaldbosma@gmail.com>
2019-12-11 18:14:17 +0100

    Changed package agent back to ubuntu
    Cgent doesn't seem to be issue when downloading module using script

--- scripts/save-vststasksdk-module.ps1
+++ scripts/save-vststasksdk-module.ps1
@@ -1,11 +1,20 @@
-New-Item "./InstallNetCoreRuntimeAndHostingTask/ps_modules" -ItemType Directory
-$moduleVersion = (Find-Module VstsTaskSdk).Version.ToString()
-Save-Module -Name VstsTaskSdk -LiteralPath ".\InstallNetCoreRuntimeAndHostingTask\ps_modules"
+Remove-Item "InstallNetCoreRuntimeAndHostingTask" -Force -Confirm
+
+
+
+$psModulesDirectory = New-Item "./InstallNetCoreRuntimeAndHostingTask/ps_modules" -ItemType Directory
+
+Write-Host $psModulesDirectory.FullName
+
+Save-Module -Name VstsTaskSdk -LiteralPath $psModulesDirectory.FullName
 
 # The PowerShell module is saved in a subfolder of VstsTaskSdk with the version as the folder name.
 # The Azure Pipeline Task expects the module to be in the root VstsTaskSdk folder, so we move the files there.
-$versionPath = Join-Path "./InstallNetCoreRuntimeAndHostingTask/ps_modules/VstsTaskSdk" $moduleVersion
+
+$moduleVersion = (Find-Module VstsTaskSdk).Version.ToString()
+$versionPath = Join-Path $psModulesDirectory.FullName "VstsTaskSdk" $moduleVersion
+
 if (Test-Path $versionPath -PathType Container)
 {
-  Move-Item "$versionPath/*" "./InstallNetCoreRuntimeAndHostingTask/ps_modules/VstsTaskSdk"
+    Move-Item "$versionPath/*" "./InstallNetCoreRuntimeAndHostingTask/ps_modules/VstsTaskSdk"
 }
