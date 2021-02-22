fd7ab6137eb7bdecb41ce285adc2850ce23f255d
Ronald Bosma <ronaldbosma@outlook.com>
2021-02-22 16:56:33 +0100

    First version of register server script

---
+++ tests/register-server-in-environment.ps1
@@ -0,0 +1,64 @@
+param (
+    [Parameter(Mandatory)][string]$OrganizationUrl,
+    [Parameter(Mandatory)][string]$TeamProject,
+    [Parameter(Mandatory)][string]$Environment,
+    [Parameter(Mandatory)][string]$Token
+)
+
+$ErrorActionPreference="Stop";
+If(-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent() ).IsInRole( [Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+    throw "Run command in an administrator PowerShell prompt"
+};
+
+If($PSVersionTable.PSVersion -lt (New-Object System.Version("3.0")))
+{
+    throw "The minimum version of Windows PowerShell that is required by the script (3.0) does not match the currently running version of Windows PowerShell."
+};
+
+If(-NOT (Test-Path $env:SystemDrive\'azagent'))
+{
+    mkdir $env:SystemDrive\'azagent'
+};
+
+cd $env:SystemDrive\'azagent';
+
+# Create a unique A* folder for the agent using i as the index
+for($i=1; $i -lt 100; $i++)
+{
+    $destFolder="A"+$i.ToString();
+    if(-NOT (Test-Path ($destFolder)))
+    {
+        mkdir $destFolder;
+        cd $destFolder;
+        break;
+    }
+};
+
+$agentZip="$PWD\agent.zip";
+
+# Configure the web client used to download the zip file with the agent
+$DefaultProxy=[System.Net.WebRequest]::DefaultWebProxy;
+$securityProtocol=@();
+$securityProtocol+=[Net.ServicePointManager]::SecurityProtocol;
+$securityProtocol+=[Net.SecurityProtocolType]::Tls12;
+[Net.ServicePointManager]::SecurityProtocol=$securityProtocol;
+$WebClient=New-Object Net.WebClient; 
+$Uri='https://vstsagentpackage.azureedge.net/agent/2.181.2/vsts-agent-win-x64-2.181.2.zip';
+if($DefaultProxy -and (-not $DefaultProxy.IsBypassed($Uri)))
+{
+    $WebClient.Proxy= New-Object Net.WebProxy($DefaultProxy.GetProxy($Uri).OriginalString, $True);
+};
+
+# Download the zip file with the agent
+$WebClient.DownloadFile($Uri, $agentZip);
+
+# Extract the zip file
+Add-Type -AssemblyName System.IO.Compression.FileSystem;
+[System.IO.Compression.ZipFile]::ExtractToDirectory( $agentZip, "$PWD");
+
+# Register the agent in the environment
+.\config.cmd --environment --environmentname $Environment --agent $env:COMPUTERNAME --runasservice --work '_work' --url $OrganizationUrl --projectname $TeamProject --auth PAT --token $Token;
+
+# Remove the zip file
+Remove-Item $agentZip;
