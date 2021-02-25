7a77cc59f0513ea4a2ba97934c8a8ccdb36af891
Ronald Bosma <ronald.bosma@infosupport.com>
2021-02-25 16:48:44 +0100

    Reuse webclient for all web calls and fix issue with selecting download url

--- tests/register-server-in-environment.ps1
+++ tests/register-server-in-environment.ps1
@@ -1,13 +1,13 @@
 <#
     .SYNOPSIS
-    Register server in Azure Piplines Environment.
+    Register server in Azure Pipelines Environment.
 
     .DESCRIPTION
     Downloads and installs agent on this server and then registers the server in an Azure Pipelines Environment.
-    This script is based on the registration script that you can copy from the Azure DevOps portal when manually adding a Virtual machines resource to an environment.
+    This script is based on the registration script that you can copy from the Azure DevOps portal when manually adding a Virtual machine resource to an environment.
 
     .PARAMETER OrganizationUrl
-    URL of the server. For example: https://myaccount.visualstudio.com or http://onprem:8080/tfs.
+    URL of the organization. For example: https://myaccount.visualstudio.com or http://onprem:8080/tfs.
 
     .PARAMETER TeamProject
     Name of the team project. For example myProject.
@@ -35,6 +35,7 @@ param (
     [string]$Tags
 )
 
+
 $ErrorActionPreference="Stop";
 If(-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent() ).IsInRole( [Security.Principal.WindowsBuiltInRole] "Administrator"))
 {
@@ -50,6 +51,7 @@ If(-NOT (Test-Path $env:SystemDrive\'azagent'))
 {
     mkdir $env:SystemDrive\'azagent'
 };
+
 
 cd $env:SystemDrive\'azagent';
 
@@ -68,22 +70,6 @@ for($i=1; $i -lt 100; $i++)
 $agentZip="$PWD\agent.zip";
 
 
-# Retrieve list with releases for the Azure Pipelines agent
-$releasesUrl = "https://api.github.com/repos/Microsoft/azure-pipelines-agent/releases"
-
-$webClient1 = New-Object System.Net.WebClient
-$webClient1.Headers.Add("user-agent", "azure pipeline");
-$releases = $webClient1.DownloadString($releasesUrl) | ConvertFrom-Json
-
-# Select the newest agent release
-$latestAgentRelease = $releases | Sort-Object -Property published_at -Descending | Select-Object -First 1
-$assetsUrl = $latestAgentRelease.assets[0].browser_download_url
-
-# Get the agent download url from the agent release assets
-$assets = $webClient1.DownloadString($assetsUrl) | ConvertFrom-Json
-$downloadUrl = $assets | Where-Object { $_.platform -eq "win-x64"} | Select-Object -First 1 -Property downloadUrl
-
-
 # Configure the web client used to download the zip file with the agent
 $DefaultProxy=[System.Net.WebRequest]::DefaultWebProxy;
 $securityProtocol=@();
@@ -91,19 +77,45 @@ $securityProtocol+=[Net.ServicePointManager]::SecurityProtocol;
 $securityProtocol+=[Net.SecurityProtocolType]::Tls12;
 [Net.ServicePointManager]::SecurityProtocol=$securityProtocol;
 $WebClient=New-Object Net.WebClient; 
-$Uri=downloadUrl;
+$WebClient.Headers.Add("user-agent", "azure pipeline");
+
+
+## Retrieve list with releases for the Azure Pipelines agent
+$releasesUrl = "https://api.github.com/repos/Microsoft/azure-pipelines-agent/releases"
+if($DefaultProxy -and (-not $DefaultProxy.IsBypassed($releasesUrl)))
+{
+    $WebClient.Proxy= New-Object Net.WebProxy($DefaultProxy.GetProxy($releasesUrl).OriginalString, $True);
+};
+$releases = $WebClient.DownloadString($releasesUrl) | ConvertFrom-Json
+
+
+## Select the newest agent release
+$latestAgentRelease = $releases | Sort-Object -Property published_at -Descending | Select-Object -First 1
+$assetsUrl = $latestAgentRelease.assets[0].browser_download_url
+
+
+## Get the agent download url from the agent release assets
+if($DefaultProxy -and (-not $DefaultProxy.IsBypassed($assetsUrl)))
+{
+    $WebClient.Proxy= New-Object Net.WebProxy($DefaultProxy.GetProxy($assetsUrl).OriginalString, $True);
+};
+$assets = $WebClient.DownloadString($assetsUrl) | ConvertFrom-Json
+$Uri = $assets | Where-Object { $_.platform -eq "win-x64"} | Select-Object -First 1 | Select-Object -ExpandProperty downloadUrl
+
+
+# Download the zip file with the agent
 if($DefaultProxy -and (-not $DefaultProxy.IsBypassed($Uri)))
 {
     $WebClient.Proxy= New-Object Net.WebProxy($DefaultProxy.GetProxy($Uri).OriginalString, $True);
 };
-
-# Download the zip file with the agent
 Write-Host "Download agent zip file from $Uri";
 $WebClient.DownloadFile($Uri, $agentZip);
+
 
 # Extract the zip file
 Add-Type -AssemblyName System.IO.Compression.FileSystem;
 [System.IO.Compression.ZipFile]::ExtractToDirectory( $agentZip, "$PWD");
+
 
 # Register the agent in the environment
 if ([string]::IsNullOrWhiteSpace($Tags))
@@ -114,6 +126,7 @@ else
 {
     .\config.cmd --unattended --environment --environmentname $Environment --agent $env:COMPUTERNAME --runasservice --work '_work' --url $OrganizationUrl --projectname $TeamProject --auth PAT --token $Token --addvirtualmachineresourcetags --virtualmachineresourcetags "$($Tags)";
 }
+
 
 # Remove the zip file
 Remove-Item $agentZip;
