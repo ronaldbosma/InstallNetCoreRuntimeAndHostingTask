e62fec2245b0fae5d681792d2e81aa554363df4d
Ronald Bosma <ronald.bosma@infosupport.com>
2021-02-26 20:08:24 +0100

    Delete azdo environment Unregister is disabled because it's not needed anymore.

--- tests/scripts/delete-azure-vms.ps1
+++ tests/scripts/delete-azure-vms.ps1
@@ -22,25 +22,44 @@ param (
 )
 
 $environmentName = $Environment.Replace(".", "-"); # Azure DevOps doesn't allow . in the name of an environment so we replace it with a -
-$unregisterServerScript = "https://raw.githubusercontent.com/ronaldbosma/InstallNetCoreRuntimeAndHostingTask/automated-test-pipeline/tests/scripts/unregister-server-from-environment.ps1";
+# $unregisterServerScript = "https://raw.githubusercontent.com/ronaldbosma/InstallNetCoreRuntimeAndHostingTask/automated-test-pipeline/tests/scripts/unregister-server-from-environment.ps1";
 
 $ErrorActionPreference="Stop";
 
-# Get all servers in the resource group
-$vms = az vm list --resource-group $environmentName | ConvertFrom-Json | Select-Object -ExpandProperty name;
+Write-Host "Retrieve id of environment $environmentName from Azure DevOps team project $TeamProject"
+$environmentId = az devops invoke `
+    --area distributedtask `
+    --resource environments `
+    --route-parameters project=$TeamProject `
+    --org $OrganizationUrl `
+    --api-version "6.0-preview" `
+    --query "value[?name=='$environmentName'].id" `
+    --output tsv
 
-# Unregister each server from the Azure Pipelines environment
-foreach ($vm in $vms)
-{
-    Write-Host "Unregister $vm from environment";
-    $unregisterServerSettings="{`\`"fileUris`\`":[`\`"$unregisterServerScript`\`"], `\`"commandToExecute`\`":`\`"powershell.exe ./unregister-server-from-environment.ps1 -Token '$Token'`\`"}";
-    az vm extension set `
-        --name CustomScriptExtension `
-        --publisher Microsoft.Compute `
-        --vm-name $vm `
-        --resource-group $environmentName `
-        --settings $unregisterServerSettings;
-}
+Write-Host "Delete environment $environmentName with id $environmentId from Azure DevOps team project $TeamProject"
+az devops invoke `
+    --area distributedtask `
+    --resource environments `
+    --route-parameters project=$TeamProject environmentId=$environmentId `
+    --org $OrganizationUrl `
+    --http-method DELETE `
+    --api-version "6.0-preview"
+
+# # Get all servers in the resource group
+# $vms = az vm list --resource-group $environmentName | ConvertFrom-Json | Select-Object -ExpandProperty name;
+
+# # Unregister each server from the Azure Pipelines environment
+# foreach ($vm in $vms)
+# {
+#     Write-Host "Unregister $vm from environment";
+#     $unregisterServerSettings="{`\`"fileUris`\`":[`\`"$unregisterServerScript`\`"], `\`"commandToExecute`\`":`\`"powershell.exe ./unregister-server-from-environment.ps1 -Token '$Token'`\`"}";
+#     az vm extension set `
+#         --name CustomScriptExtension `
+#         --publisher Microsoft.Compute `
+#         --vm-name $vm `
+#         --resource-group $environmentName `
+#         --settings $unregisterServerSettings;
+# }
 
 Write-Host "Delete resource group $environmentName"
 az group delete --name $environmentName --no-wait --yes
